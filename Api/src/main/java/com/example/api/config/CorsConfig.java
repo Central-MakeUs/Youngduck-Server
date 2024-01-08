4cec920e8c06351d01beeb9b0fbeee18bdb04a86
Hanbee Lee <bee1162@naver.com>
2024-01-08 17:09:16 +0900

    fix:deploy https변경

--- Api/src/main/java/com/example/api/config/CorsConfig.java
+++ Api/src/main/java/com/example/api/config/CorsConfig.java
@@ -18,6 +18,9 @@ public class CorsConfig implements WebMvcConfigurer {
         allowedOriginPatterns.add("http://localhost:8080");
         allowedOriginPatterns.add("http://3.35.246.60");
         allowedOriginPatterns.add("http://3.35.246.60:8080");
+        allowedOriginPatterns.add("http://popcornmate.p-e.kr");
+        allowedOriginPatterns.add("https://popcornmate.p-e.kr");
+        allowedOriginPatterns.add("https://www.popcornmate.p-e.kr");
 
         String[] patterns = allowedOriginPatterns.toArray(String[]::new);
         registry.addMapping("/**")
