d5d9b50d9097bea25d0ba73d4f64874aa97f5cee
Hanbee Lee <bee1162@naver.com>
2024-01-22 16:40:49 +0900

    feat: UserScreening에 신고횟수 추가

--- Domain/src/main/java/com/example/domains/userscreening/entity/UserScreening.java
+++ Domain/src/main/java/com/example/domains/userscreening/entity/UserScreening.java
@@ -22,6 +22,7 @@ public class UserScreening{
     private boolean isHost;
     private boolean isBookmarked;
     private boolean isParticipating;
+    private int complaintCount;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
