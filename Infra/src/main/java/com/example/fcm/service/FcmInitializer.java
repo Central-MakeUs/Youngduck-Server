62be274f6f0df6ee20f077f6a71ce5a439815e89
Hanbee Lee <bee1162@naver.com>
2024-01-23 22:00:47 +0900

    setting: fcm initializer구현

---
+++ Infra/src/main/java/com/example/fcm/service/FcmInitializer.java
@@ -0,0 +1,41 @@
+package com.example.fcm.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FcmInitializer {
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource(firebaseConfigPath);
+        InputStream resourceInputStream = resource.getInputStream();
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> apps = FirebaseApp.getApps();
+
+        if (apps != null && !apps.isEmpty()) {
+            for (FirebaseApp app : apps) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
+            }
+        } else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(resourceInputStream)).build();
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
