87b0127d202d53ee7c34986a55dc4f6294e413a8
Hanbee Lee <bee1162@naver.com>
2024-01-28 23:56:13 +0900

    fix: 팝콘작 리뷰 가져올때 어떤 팝콘작에 대한 팝콘리뷰인지 popcornId도 함께 반환으로 변경

--- Domain/src/main/java/com/example/domains/popcorn/schedule/PopcornSchedule.java
+++ Domain/src/main/java/com/example/domains/popcorn/schedule/PopcornSchedule.java
@@ -13,11 +13,11 @@ import java.util.List;
 public class PopcornSchedule {
     private final PopcornService popcornService;
 
-    // 매 주 일요일 0시 0분에 실행되도록 cron 설정
-    @Scheduled(cron = "0 0 0 * * SUN")
+    // 매 주 일요일 0시 0분에 실행되도록 cron 설정-ㄱㄷ미
+    @Scheduled(cron = "0 0 0 * * MON")
     public void scheduledMethod() {
         popcornService.getTopRecommended();
-        System.out.println("매 주 일요일에 실행되는 작업");
+        System.out.println("매 주 월요일에 실행되는 작업");
     }
 
 //        @Scheduled(cron = "0 * * * * *")
